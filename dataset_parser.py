398928247b67a9aa7cc4f37733eff0f5bd2231a5
Kevin giannandrea <giannandreak79@gmail.com>
2025-04-19 09:32:01 +0000

    Aggiunta classi per parsing del dataset

---
+++ dataset_parser.py
@@ -0,0 +1,165 @@
+from collections import Counter
+import math
+from difflib import SequenceMatcher
+
+class DatasetParser:
+
+  def __init__(self, entity_id, sitelinks, claims, label, description):
+
+    self.entity_id = entity_id
+    self.sitelinks = sitelinks
+    self.claims = claims
+    self.label = label
+    self.description = description
+
+  def claims_target(self, properties_to_check: dict = {}) -> dict:
+    """
+    Restituisce i claims target che contengono le properties specificate.
+    """
+    if properties_to_check == {}:
+        return {}
+
+    target_claims = {}
+
+    for property_id, data_value in self.claims.items():
+        if property_id in properties_to_check:
+            target_claims[property_id] = data_value
+
+    return target_claims
+
+  def get_claims_value(self, target_claims) -> dict:
+    """
+    Restituisce i claims con i relativi valori.
+    ex. Country of Origin (P495): ['Q38']
+    """
+
+    if target_claims == {}:
+        return {}
+
+    claims_value = {}
+    for property_id, data_value in target_claims.items():
+        claims_value[property_id] = []
+        for value in data_value:
+            datavalue = value["mainsnak"]["datavalue"]
+            if datavalue["type"] == "wikibase-entityid":
+                claims_value[property_id].append(datavalue["value"]["id"])  # Estrae il QID (es. 'Q38' per Italy)
+            elif datavalue["type"] == "string":
+                claims_value[property_id].append(datavalue["value"])  # Estrae stringhe
+            elif datavalue["type"] == "quantity":
+                claims_value[property_id].append(datavalue["value"]["amount"])  # Estrae quantità numeriche
+
+    return claims_value
+
+  @staticmethod
+  def get_similarity(s1, s2):
+    """Calcola similarità di due stringhe usando SequenceMatcher (range 0-1)."""
+    if not s1 or not s2:
+        return 0.0
+
+    # 2. Calcolo della similarità
+    #    Crea un oggetto SequenceMatcher con le due stringhe.
+    #    Il primo argomento 'None' indica di non ignorare nessun carattere "spazzatura".
+    #    Chiama il metodo .ratio() per ottenere il punteggio di similarità.
+    return SequenceMatcher(None, s1, s2).ratio()
+
+  def sitelinks_translation_entropy(self, similarity_threshold: float = 0.9) -> float:
+      """
+      Restituisce l'entropia sulla distrubuzione dei titoli per comprendere se dato un item, corrispondono piu'
+      traduzioni nelle diverse lingue.
+      Un valore vicino a 0 indica alta dominanza di uno/pochi titoli (bassa diversità).
+      Un valore alto indica alta diversità di titoli.
+      ex. "pasta alla gricia" in molte lingue non ha nessuna traduzione (quindi avra' una bassa entropia), questo puo' significare un item cultural exclusive.
+      """
+
+      if self.sitelinks == {}:
+        return None
+
+      entropy = 0.0
+      cleaned_titles = []
+      for lang, value in self.sitelinks.items():
+        if lang == "commonswiki" or not lang.endswith("wiki"):
+          continue
+
+        title = value.get("title", {})
+        cleaned_titles.append(title.lower())
+
+      if cleaned_titles == []:
+        return entropy
+
+      title_counts = Counter(cleaned_titles)
+      total_titles = sum(title_counts.values())
+      num_unique_titles = len(title_counts)
+      if num_unique_titles <= 1:
+        return entropy
+
+      sorted_unique_titles = [title for title, count in title_counts.most_common()]
+      final_grouped_counts = Counter()
+      processed_mask = {title: False for title in sorted_unique_titles}
+
+      for i, representative_norm in enumerate(sorted_unique_titles):
+          if processed_mask[representative_norm]:
+              continue
+
+          processed_count = 0
+          current_group_count = title_counts[representative_norm]
+          processed_mask[representative_norm] = True
+          processed_count += title_counts[representative_norm]
+
+          for j in range(i + 1, len(sorted_unique_titles)):
+                other_norm = sorted_unique_titles[j]
+                if processed_mask[other_norm]:
+                    continue
+
+                similarity = self.get_similarity(representative_norm, other_norm)
+                if similarity >= similarity_threshold:
+                    current_group_count += title_counts[other_norm]
+                    processed_mask[other_norm] = True
+                    processed_count += title_counts[other_norm]
+
+          final_grouped_counts[representative_norm] = current_group_count
+
+      for title in final_grouped_counts:
+        probability = final_grouped_counts[title] / total_titles
+        entropy -= probability * math.log2(probability)
+
+      #per normalizzare il valore tra 0 e 1
+      #max_entropy = math.log2(num_unique_titles)
+      #entropy_norm = entropy / max_entropy
+
+      return entropy
+
+  def get_number_sitelinks(self) -> int:
+    """
+    Restituisce il numero di sitelinks.
+    """
+    return len(self.sitelinks)
+
+
+if __name__ == '__main__':
+    # Esempio di utilizzo
+    wikidata_url = "https://www.wikidata.org/wiki/Q55641393"
+    entity = WikidataExtractor(wikidata_url)
+    properties_to_check = {
+        "P495": "Country of Origin",
+        "P17": "Country",
+        "P131": "Located in Adm. Entity",
+        "P361": "Part Of",
+        "P31": "Instance Of",
+        "P279": "Subclass Of",
+        "P443": "Pronuncation audio",
+        "P1435": "Heritage designation",
+        "P27": "Country of citizenship",
+        "P1705": "Native Label",
+        "P2012": "Cuisine"
+    }
+
+    dataset_parser = DatasetParser(entity.get_entity_id(), entity.get_sitelinks(), entity.get_claims(), entity.get_label(), entity.get_description())
+    target_claims = dataset_parser.claims_target(properties_to_check)
+    claims_value = dataset_parser.get_claims_value(target_claims)
+    number_sitelinks = dataset_parser.get_number_sitelinks()
+    sitelinks_translation_entropy = dataset_parser.sitelinks_translation_entropy()
+
+    print(target_claims)
+    print(claims_value)
+    print(number_sitelinks)
+    print(sitelinks_translation_entropy)
---
+++ wiki_extractor.py
@@ -0,0 +1,112 @@
+from wikidata.client import Client
+import requests
+
+class WikidataExtractor:
+    """
+    Classe per estrarre informazioni da Wikidata a partire da un URL o un ID di entità.
+    """
+    def __init__(self, identifier):
+        # Inizializza il client e carica l'item
+        self.client = Client()
+        self.entity_id = self.extract_entity_id(identifier)
+        self.item = self._fetch_item(self.entity_id)
+        self.url_api = "https://en.wikipedia.org/w/api.php"
+
+    @staticmethod
+    def extract_entity_id(url_or_id: str) -> str:
+        """
+        Estrae l'ID di entità (es. Q42) da un URL di Wikidata o restituisce l'ID se è già fornito.
+        """
+        if url_or_id.startswith(('http://', 'https://')):
+            return url_or_id.strip().split("/")[-1]
+        return url_or_id
+
+    def request_data(self, title: str = "enwiki"):
+        """
+        Richiesta dati da Wikipedia tramite API, ritorna il contenuto della pagina.
+        """
+        params = {
+          "action": "query",
+          "prop": "extracts",
+          "explaintext": True,
+          "titles": title,
+          "format": "json",
+          "redirects": 1
+        }
+
+        try:
+              res = requests.get(self.url_api, params=params).json()
+              page = next(iter(res["query"]["pages"].values()))
+              text = page.get("extract", "")
+              return text
+        except Exception as e:
+              print(e)
+              return None
+
+    def _fetch_item(self, entity_id: str):
+        """
+        Recupera l'item da Wikidata, caricando etichette, descrizioni e sitelinks.
+        """
+        try:
+            return self.client.get(entity_id, load=True)
+        except Exception as e:
+            raise ValueError(f"Errore nel recupero dell'entità {entity_id}: {e}")
+
+    def get_entity_id(self) -> str:
+        """
+        Restituisce l'etichetta dell'entità nella lingua specificata (default: en).
+        """
+        return self.entity_id
+
+    def get_label(self) -> str:
+        """
+        Restituisce l'etichetta dell'entità nella lingua specificata (default: en).
+        """
+        return self.item.label
+
+    def get_description(self) -> str:
+        """
+        Restituisce la descrizione dell'entità nella lingua specificata (default: en).
+        """
+        return self.item.description
+
+    def get_sitelinks(self):
+        """
+        Restituisce un dizionario dei sitelink di Wikipedia:
+          {lang: url, ...}
+        """
+        raw = self.item.data.get('sitelinks', {})
+        result = {}
+        for site_key, site_data in raw.items():
+            if site_key.endswith('wiki') and not site_key.startswith('commons'):
+                lang = site_key.replace('wiki', '')
+                title = site_data.get('title', '')
+                url = f"https://{lang}.wikipedia.org/wiki/{title.replace(' ', '_')}"
+                result[site_key] = {'url': url, 'title': title}
+
+        return result
+
+    def get_claims(self) -> dict:
+        """
+        Restituisce tutte le claim dell'entità come dict:
+        {property_id: [valore1, valore2, ...], ...}
+        """
+        raw = self.item.data.get('claims', {})
+        claims = {}
+        for prop, cl_list in raw.items():
+            prop_id = prop
+            values = cl_list
+            claims[prop_id] = values
+        return claims
+
+
+
+if __name__ == '__main__':
+    # Esempio di utilizzo
+    wikidata_url = "https://www.wikidata.org/wiki/Q55641393"
+    entity = WikidataExtractor(wikidata_url)
+    print("Entity ID:", entity.get_entity_id())
+    print("Label:", entity.get_label())
+    print("Description:", entity.get_description())
+    print("Sitelinks:", entity.get_sitelinks())
+    print("Claims:", entity.get_claims())
